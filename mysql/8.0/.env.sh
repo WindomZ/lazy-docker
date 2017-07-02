05811ae4a33359d2223f1e9df7e2ee67842eadc5
WindomZ <git.windomz@gmail.com>
2017-07-03 01:08:26 +0800

    feat(mysql5.8): rebuild code with shellcheck

--- mysql/8.0/.env.sh
+++ mysql/8.0/.env.sh
@@ -1,18 +1,44 @@
 #!/usr/bin/env bash
 # specifies the configuration file.
 env=.env
-[[ ! -f ${env} ]] && env=../.env
-[[ ! -f ${env} ]] && echo -e "\033[31mNot found\033[0m the \033[4m.env\033[0m file" && exit 1
+[[ ! -f "$env" ]] && env=../.env
+[[ ! -f "$env" ]] && echo -e "\033[31mNot found\033[0m the \033[4m.env\033[0m file" && exit 1
 
 # specifies the name of docker container.
-name=`sed '/^DOCKER_NAME=/!d;s/.*=//' ${env}`
-[[ ! ${name} ]] && read -e -p "Enter the name of docker container: [mysql8.0] " name
-[[ ! ${name} ]] && name="mysql8.0"
+name=$(sed '/^DOCKER_NAME=/!d;s/.*=//' "$env")
+[[ ! "$name" ]] && read -erp "Enter the name of docker container: [mysql8.0] " name
+[[ ! "$name" ]] && name="mysql8.0"
 
 # specifies the MYSQL data storage path.
-volume=`sed '/^DOCKER_VOLUME=/!d;s/.*=//' ${env}`
-[[ ! ${volume} ]] && read -e -p "Enter the directory path for the volume: [current path] " volume
-[[ ! ${volume} ]] && volume=${PWD}
+volume=$(sed '/^DOCKER_VOLUME=/!d;s/.*=//' "$env")
+[[ ! "$volume" ]] && read -erp "Enter the directory path for the MYSQL volume: [current path] " volume
+[[ ! "$volume" ]] && volume=${PWD}
+
+# specifies the MYSQL external port.
+port=$(sed '/^DOCKER_PORT=/!d;s/.*=//' "$env")
+[[ ! "$port" ]] && read -erp "Enter the MYSQL host port: [3306] " port
+[[ ! "$port" ]] && port=3306
+
+# specifies the password that will be set for the MySQL root superuser account.
+root_password=$(sed '/^MYSQL_ROOT_PASSWORD=/!d;s/.*=//' "$env")
+[[ ! "$root_password" ]] && read -erp "Enter the MySQL root password: " root_password
+
+# specifies other environment Variables.
+mysql_database=$(sed '/^MYSQL_DATABASE=/!d;s/.*=//' "$env")
+mysql_user=$(sed '/^MYSQL_USER=/!d;s/.*=//' "$env")
+mysql_password=$(sed '/^MYSQL_PASSWORD=/!d;s/.*=//' "$env")
+mysql_empty_password=$(sed '/^MYSQL_ALLOW_EMPTY_PASSWORD=/!d;s/.*=//' "$env")
+mysql_random_password=$(sed '/^MYSQL_RANDOM_ROOT_PASSWORD=/!d;s/.*=//' "$env")
+mysql_onetime_password=$(sed '/^MYSQL_ONETIME_PASSWORD=/!d;s/.*=//' "$env")
+[[ ! "$root_password" ]] && mysql_empty_password=yes && unset mysql_random_password
 
 export env
 export volume
+export port
+export root_password
+export mysql_database
+export mysql_user
+export mysql_password
+export mysql_empty_password
+export mysql_random_password
+export mysql_onetime_password
--- mysql/8.0/run.sh
+++ mysql/8.0/run.sh
@@ -1,55 +1,39 @@
 #!/usr/bin/env bash
-source ./.env.sh
-
-# specifies the MySQL port.
-port=`sed '/^DOCKER_PORT=/!d;s/.*=//' ${env}`
-[[ ! ${port} ]] && read -e -p "Enter the MySQL port: [3306] " port
-[[ ! ${port} ]] && port=3306
-
-# specifies the password that will be set for the MySQL root superuser account.
-root_password=`sed '/^MYSQL_ROOT_PASSWORD=/!d;s/.*=//' ${env}`
-[[ ! ${root_password} ]] && read -e -p "Enter the MySQL root password: " root_password
-
-# specifies other environment Variables.
-mysql_database=`sed '/^MYSQL_DATABASE=/!d;s/.*=//' ${env}`
-mysql_user=`sed '/^MYSQL_USER=/!d;s/.*=//' ${env}`
-mysql_password=`sed '/^MYSQL_PASSWORD=/!d;s/.*=//' ${env}`
-mysql_empty_password=`sed '/^MYSQL_ALLOW_EMPTY_PASSWORD=/!d;s/.*=//' ${env}`
-mysql_random_password=`sed '/^MYSQL_RANDOM_ROOT_PASSWORD=/!d;s/.*=//' ${env}`
-mysql_onetime_password=`sed '/^MYSQL_ONETIME_PASSWORD=/!d;s/.*=//' ${env}`
-[[ ! ${root_password} ]] && mysql_empty_password="yes" && unset mysql_random_password
+# shellcheck disable=SC1091,SC2015,SC2154
+[[ ! -r '.env.sh' ]] && (echo 'Not found .env file') && exit 1
+source .env.sh
 
 # remove mysql8.0 container
-if [ "$(echo "$(docker ps -aq -f name=${name} --format "{{.Names}}")" | grep -i "^${name}$")" ]; then
-  docker rm -f ${name} > /dev/null
+if docker ps -aq -f name="$name" --format "{{.Names}}" | grep -iq "^$name$"; then
+  docker rm -f "$name" > /dev/null
 fi
 
 # run mysql8.0-db container
-if [ ! "$(docker ps -q -f name=${name}-db)" ]; then
-  if [ ! "$(docker ps -aq -f status=exited -f name=${name}-db)" ]; then
+if ! docker ps -q -f name="$name-db" --format "{{.Names}}" | grep -iq "^$name-db$"; then
+  if ! docker ps -aq -f status=exited -f name="$name-db" --format "{{.Names}}" | grep -iq "^$name-db$"; then
     docker run \
-      -v ${volume}/${name}/data:/var/lib/mysql \
-      --name ${name}-db mysql:8.0 \
+      -v "$volume/$name/data":/var/lib/mysql \
+      --name "$name-db" mysql:8.0 \
       echo "Data-only container for mysql" > /dev/null
 
-    echo -e "Created \033[4m${name}-db\033[0m \033[32mSuccessfully\033[0m!"
+    echo -e "Created \033[4m$name-db\033[0m \033[32mSuccessfully\033[0m!"
   fi
 fi
 
 # run mysql8.0 container
 docker run --restart=always \
-  --volumes-from ${name}-db \
-  -v ${volume}/${name}/conf:/etc/mysql/conf.d \
-  -v ${volume}/${name}/logs:/log \
-  -v ${volume}/${name}/share:/share \
-  -p ${port}:3306 \
-  -e MYSQL_ROOT_PASSWORD=${root_password} \
-  -e MYSQL_DATABASE=${mysql_database} \
-  -e MYSQL_USER=${mysql_user} \
-  -e MYSQL_PASSWORD=${mysql_password} \
-  -e MYSQL_ALLOW_EMPTY_PASSWORD=${mysql_empty_password} \
-  -e MYSQL_RANDOM_ROOT_PASSWORD=${mysql_random_password} \
-  -e MYSQL_ONETIME_PASSWORD=${mysql_onetime_password} \
-  --name ${name} -d mysql:8.0 > /dev/null
+  --volumes-from "$name-db" \
+  -v "$volume/$name/conf":/etc/mysql/conf.d \
+  -v "$volume/$name/logs":/log \
+  -v "$volume/$name/share":/share \
+  -p "$port":3306 \
+  -e MYSQL_ROOT_PASSWORD="$root_password" \
+  -e MYSQL_DATABASE="$mysql_database" \
+  -e MYSQL_USER="$mysql_user" \
+  -e MYSQL_PASSWORD="$mysql_password" \
+  -e MYSQL_ALLOW_EMPTY_PASSWORD="$mysql_empty_password" \
+  -e MYSQL_RANDOM_ROOT_PASSWORD="$mysql_random_password" \
+  -e MYSQL_ONETIME_PASSWORD="$mysql_onetime_password" \
+  --name "$name" -d mysql:8.0 > /dev/null
 
-echo -e "Created \033[4m${name}\033[0m \033[32mSuccessfully\033[0m!"
+echo -e "Created \033[4m$name\033[0m \033[32mSuccessfully\033[0m!"
