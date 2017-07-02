f9b00880d8f61df654ddc5d4eeaade63c313460f
WindomZ <git.windomz@gmail.com>
2017-07-02 18:26:31 +0800

    feat(nginx): check code with shellcheck

--- nginx/1.13/.env.sh
+++ nginx/1.13/.env.sh
@@ -5,18 +5,18 @@ env=.env
 [[ ! -f ${env} ]] && echo -e "\033[31mNot found\033[0m the \033[4m.env\033[0m file" && exit 1
 
 # specifies the name of docker container.
-name=`sed '/^DOCKER_NAME=/!d;s/.*=//' ${env}`
-[[ ! ${name} ]] && read -e -p "Enter the name of docker container: [nginx1.13] " name
+name="sed '/^DOCKER_NAME=/!d;s/.*=//' ${env}"
+[[ ! ${name} ]] && read -erp "Enter the name of docker container: [nginx1.13] " name
 [[ ! ${name} ]] && name="nginx1.13"
 
 # specifies the nginx host path.
-volume=`sed '/^DOCKER_VOLUME=/!d;s/.*=//' ${env}`
-[[ ! ${volume} ]] && read -e -p "Enter the directory path for the nginx volume: [current path] " volume
+volume="sed '/^DOCKER_VOLUME=/!d;s/.*=//' ${env}"
+[[ ! ${volume} ]] && read -erp "Enter the directory path for the nginx volume: [current path] " volume
 [[ ! ${volume} ]] && volume=${PWD}
 
 # specifies the nginx external host port.
-port=`sed '/^DOCKER_PORT=/!d;s/.*=//' ${env}`
-[[ ! ${port} ]] && read -e -p "Enter the nginx host port: [80] " port
+port="sed '/^DOCKER_PORT=/!d;s/.*=//' ${env}"
+[[ ! ${port} ]] && read -erp "Enter the nginx host port: [80] " port
 [[ ! ${port} ]] && port=80
 
 export env
--- nginx/1.13/run.sh
+++ nginx/1.13/run.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC1091,SC2015,SC2154
+[[ ! -r '.env.sh' ]] && (echo 'Not found .env file') && exit 1
 source .env.sh
 
 # whether or not to import nginx.conf
 if [[ -s 'nginx.conf' ]]; then
   while true; do
-    read -e -p "Do you use nginx.conf?: [Y/n] " yn
+    read -erp "Do you use nginx.conf?: [Y/n] " yn
     case ${yn} in
       [Yy]*|'' ) volume_nginx_conf=true; break;;
       [Nn]* ) break;;
-      * ) echo "Please answer yes or no.";;
+      * ) echo "Please answer yes, y, no, or n.";;
     esac
   done
 fi
@@ -22,41 +24,42 @@ for file in ./conf.d/*; do
 done
 if [[ ${volume_nginx_conf_d} ]]; then
   while true; do
-    read -e -p "Do you overwrite nginx/conf.d?: [Y/n] " yn
+    read -erp "Do you overwrite nginx/conf.d?: [Y/n] " yn
     case ${yn} in
       [Yy]*|'' ) break;;
       [Nn]* ) unset volume_nginx_conf_d; break;;
-      * ) echo "Please answer yes or no.";;
+      * ) echo "Please answer yes, y, no, or n.";;
     esac
   done
 fi
 #[[ ${volume_nginx_conf_d} ]] && echo "YES" || echo "NO"
 
 # remove nginx container
-if [ "$(echo "$(docker ps -aq -f name=${name} --format "{{.Names}}")" | grep -i "^${name}$")" ]; then
-  docker rm -f ${name} > /dev/null
+if docker ps -aq -f name="$name" --format "{{.Names}}" | grep -iq "^${name}$"; then
+  docker rm -f "$name" > /dev/null
 fi
 
 [[ ${volume_nginx_conf} ]] \
-  && volume_nginx_conf="-v ${PWD}/nginx.conf:/etc/nginx/nginx.conf:ro" \
+  && volume_nginx_conf="-v $PWD/nginx.conf:/etc/nginx/nginx.conf:ro" \
   || unset volume_nginx_conf
 
 [[ ${volume_nginx_conf_d} ]] \
-  && volume_nginx_conf_d="-v ${PWD}/conf.d/:/etc/nginx/conf.d/" \
+  && volume_nginx_conf_d="-v $PWD/conf.d/:/etc/nginx/conf.d/" \
   || unset volume_nginx_conf_d
 
 # build & run nginx container
-docker run --restart=always \
-  -p ${port}:80 \
-  -v ${volume}/${name}/html:/usr/share/nginx/html:ro \
-  -v ${volume}/${name}/logs:/var/log/nginx \
-  -v ${volume}/${name}/share:/share \
-  ${volume_nginx_conf} \
-  ${volume_nginx_conf_d} \
-  --name ${name} -d nginx:1.13 > /dev/null
 
-if [ $? -ne 0 ]; then
-  exit 0
-elif [ ! -r ${volume}/${name}/html -o ! -w ${volume}/${name}/html ]; then
-  sudo chmod -R a+rw ${volume}/${name}/html
+if output=$(docker run --restart=always \
+  -p "$port":80 \
+  -v "$volume/$name/html":/usr/share/nginx/html:ro \
+  -v "$volume/$name/logs":/var/log/nginx \
+  -v "$volume/$name"/share:/share \
+  "$volume_nginx_conf" \
+  "$volume_nginx_conf_d" \
+  --name "$name" -d nginx:1.13 > /dev/null); then
+  [[ ! -r "$volume/$name"/html || ! -w "$volume/$name"/html ]] && sudo chmod -R a+rw "$volume/$name"/html
+else
+  echo -e "Created \033[4m$name\033[0m \033[31mFailed\033[0m: $output" && exit 1
 fi
+
+echo -e "Created \033[4m$name\033[0m \033[32mSuccessfully\033[0m!"
